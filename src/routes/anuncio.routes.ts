e728eb2db1a2e76b6cf28c6f6490516e2fe2ae69
RomuloFerrazChaves <romulochavesferraz@gmail.com>
2025-10-28 19:58:38 -0300

    Removendo file do anuncio (depois avaliamos a necessidade)

--- src/routes/anuncio.routes.ts
+++ src/routes/anuncio.routes.ts
@@ -8,14 +8,9 @@ import {
   updateAnuncio,
   deleteAnuncio,
 } from "../controllers/anuncioController";
-import multer from "multer";
 
 const router: Router = Router();
 
-const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 5 * 1024 * 1024 },
-});
 
 // --- ROTAS PÚBLICAS ---
 // Qualquer um pode ver a lista de anúncios e os detalhes de um anúncio
@@ -26,7 +21,7 @@ router.get("/getAnunciosByUser/:userId", getAnunciosByUser);
 // --- ROTAS PRIVADAS ---
 // Apenas usuários logados podem criar, atualizar ou deletar anúncios.
 // O middleware de autenticação é aplicado a todas as rotas abaixo.
-router.post("/createAnuncio", authMiddleware, upload.single("file"), createAnuncio);
+router.post("/createAnuncio", authMiddleware, createAnuncio);
 router.put("/updateAnuncio/:id", authMiddleware, updateAnuncio);
 router.delete("/deleteAnuncio/:id", authMiddleware, deleteAnuncio);
 
